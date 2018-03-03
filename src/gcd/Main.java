b84c4063de2d388d47bb59e037d1cd61781571de
yoona-heo <astroatom44@gmail.com>
2018-03-04 00:05:23 +0900

    최소공배수 추가

--- src/gcd/Main.java
+++ src/gcd/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) { 
-		//최대공약수 최소공배수
+		//최대공약수 최소공배수 구하기
 	
-		System.out.println("두 정수를 입력하세요: ");
+		System.out.println("두 정수를 입력하세요.");
 		Scanner scanner = new Scanner (System.in);
-		int x = scanner.nextInt(), y = scanner.nextInt(), min = 2, nextX = x, nextY = y, gdb = 1;
+		int x = scanner.nextInt(), y = scanner.nextInt(), min = 2, nextX = x, nextY = y, gcd = 1, lcm = 1;
 		boolean isMod = true;
 		if(x < y) min = x;
 		else min = y;
@@ -18,13 +18,15 @@ public class Main {
 				if(nextX % i == 0 && nextY % i == 0) {
 					nextX = nextX / i;
 					nextY = nextY / i;
-					gdb = gdb * i;
+					gcd = gcd * i;
+					lcm = gcd * nextX * nextY;
 					isMod = true;
 					break;
 				}
 			}
 		}
 		
-		System.out.println("최대공약수: " + gdb);
+		System.out.println("최대공약수: " + gcd);
+		System.out.println("최소공배수: " + lcm);
 	}
 }
